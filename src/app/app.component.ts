126f1f8abe5ce89d424717898e3e2111d991ad87
Macos <mina@Macos.local>
2022-09-25 13:32:21 +0330

    password generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'password-generator';
+  public length = 0
+  public useLetter = false
+  public useNumber = false
+  public useSymbol = false
+  public password = ''
+
+  onChangeLetter() {
+    this.useLetter = !this.useLetter
+  }
+  onChangeNumber() {
+    this.useNumber = !this.useNumber
+  }
+  onChangeSymbol() {
+    this.useSymbol = !this.useSymbol
+  }
+  onClickGenerate() {
+    let letter = 'qwertyuiopasdfghjklzxcvbnm'
+    let number = '1234567890'
+    let symbol = '!@#$%^&*'
+
+    let validChars = ''
+    if (this.useLetter) {
+      validChars += letter
+    }
+    if (this.useNumber) {
+      validChars += number
+    }
+    if (this.useSymbol) {
+      validChars += symbol
+    }
+
+    let generatorPassword = ''
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length)
+      generatorPassword += validChars[index]
+    }
+    this.password = generatorPassword
+  }
 }
